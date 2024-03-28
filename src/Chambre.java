ad53586d022c5df35d3f043879f73651b84ef77b
eyness3R <djazari.ines@outlook.com>
2024-03-28 01:42:27 +0100

    updates on room class

--- src/Chambre.java
+++ src/Chambre.java
@@ -1,3 +1,32 @@
+
+
 public class Chambre {
+
     
+    private int NumChambre;
+    private String typeChambre;
+    private boolean estDisponible;
+
+    public Chambre(int NumChambre, String typeChambre) {
+        this.NumChambre = NumChambre;
+        this.typeChambre = typeChambre;
+        this.estDisponible = true;
+    }
+
+    public int getNumeroChambre() {
+        return NumChambre;
+    }
+
+    public String getTypeChambre() {
+        return typeChambre;
+    }
+
+    public boolean estDisponible() {
+        return estDisponible;
+    }
+
+    public void setDisponibilite(boolean disponible) {
+        this.estDisponible = disponible;
+    }
 }
+
